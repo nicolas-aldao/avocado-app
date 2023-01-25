1cc516f6422d8afee2716d5d87a3319a3fa4fec0
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-01-25 15:51:22 -0300

    fixed detail screen and added cors dependency

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -14,7 +14,7 @@ const ProductPage = () => {
         .fetch(`/api/avo/${query.id}`)
         .then((response) => response.json())
         .then((data: TProduct) => {
-          setProduct(data);
+          setProduct(data.entry);
         });
     }
   }, [query.id]);
